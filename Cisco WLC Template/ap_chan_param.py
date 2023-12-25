f01492f1d02d52bf8e9218ff515d3b9132f5f075
vig0gne <82566173+vig0gne@users.noreply.github.com>
2023-12-25 15:25:05 +0500

    Update ap_chan_param.py
    Wrapped snmp queries in try-except

--- Cisco WLC Template/ap_chan_param.py
+++ Cisco WLC Template/ap_chan_param.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
+List of parameters
 :param arg[1] - Controller Address
 :param arg[2] - AP Index
 :param arg[3] - Case
 :param arg[4] - SNMP Community
 """
-import os
-os.environ['MIBS']='AIRESPACE-WIRELESS-MIB'
 
+import os
 from snmp_cmds import *
 import sys
 
-
-args = sys.argv
-query = Session(ipaddress=args[1], read_community=args[4])
+os.environ['MIBS'] = 'AIRESPACE-WIRELESS-MIB'
+# args = sys.argv
+args = ['test', '10.45.200.200', '112.107.185.129.146.224', 'NoisePower5', 'NfRcBrJvV']
+query = Session(ipaddress=args[1], read_community=args[4], timeout=10)
 interferencemib = 'AIRESPACE-WIRELESS-MIB::bsnAPIfInterferenceUtilization.'
 noisemib = 'AIRESPACE-WIRELESS-MIB::bsnAPIfDBNoisePower.'
+num_chan = 'AIRESPACE-WIRELESS-MIB::bsnAPIfPhyChannelNumber.'
 
 
 def get_channel(chrange: str):
@@ -23,7 +25,10 @@ def get_channel(chrange: str):
     :param chrange: Base frequency: '0' - is 2.4 GHz, '1' - 5 GHz
     :return:
     """
-    return query.get(oid='AIRESPACE-WIRELESS-MIB::bsnAPIfPhyChannelNumber.' + args[2] + '.' + chrange).strip('ch\n')
+    try:
+        return query.get(oid=num_chan + args[2] + '.' + chrange).strip('ch\n')
+    except:
+        pass
 
 
 def get_value(mib: str, apidx: str, chanrange: str):
@@ -33,13 +38,19 @@ def get_value(mib: str, apidx: str, chanrange: str):
     :param chanrange: Base frequency: '0' - is 2.4 GHz, '1' - 5 GHz
     :return:
     """
-    return query.get(oid=mib + apidx + '.' + chanrange + '.' + get_channel(chanrange))
+    channel = get_channel(chanrange)
+    if channel:
+        try:
+            return query.get(oid=mib + apidx + '.' + chanrange + '.' + channel)
+        except:
+            pass
 
 
-action = {'InterferenceUtil24': get_value(interferencemib, args[2], '0'),
-          'InterferenceUtil5': get_value(interferencemib, args[2], '1'),
-          'NoisePower24': get_value(noisemib, args[2], '0'),
-          'NoisePower5': get_value(noisemib, args[2], '1')}
-
-if args[3] in action:
-    print(action.get(args[3]))
+if args[3] == 'InterferenceUtil24':
+    print(get_value(interferencemib, args[2], '0'))
+elif args[3] == 'InterferenceUtil5':
+    print(get_value(interferencemib, args[2], '1'))
+elif args[3] == 'NoisePower24':
+    print(get_value(noisemib, args[2], '0'))
+elif args[3] == 'NoisePower5':
+    print(get_value(noisemib, args[2], '1'))
